7b5eb48d45d890129b5ec69d3e222bee9e61add5
jrivest2 <justinrivest@gmail.com>
2020-10-26 13:46:46 -0600

    setup and practice module

---
+++ lambdata/example_module.py
@@ -0,0 +1,15 @@
+"""Lambdata - a collection of Data Science helper functions"""
+import pandas as pd
+import numpy as np
+
+FAVORITE_NUMBERS = [77,107.32,55,12,3.14]
+
+
+def df_cleaner(df):
+    """Cleans a DF"""
+    # TODO - implement df_cleaner
+    pass
+
+def df_destroyer(df):
+    """Destroys a DF"""
+    pass
