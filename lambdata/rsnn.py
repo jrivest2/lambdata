7642d3bd406cadd44105a65367bb353c90d17eac
jrivest2 <justinrivest@gmail.com>
2020-10-27 15:59:10 -0600

    PEP8 conventions and adding Classes

--- lambdata/rsnn.py
+++ lambdata/rsnn.py
@@ -3,36 +3,37 @@ import numpy as np
 import random
 
 
-def numNull(df):
-    return df.isnull().sum().sum()
+class DF(pd.DataFrame):
 
+    def numNull(self):
+        return self.isnull().sum().sum()
 
-def randomShuffle(df):
-    df = df.copy()
-    # Make a list to hold the indecies (sorted and random)
-    indexNums = []
-    randIndecies = []
-    
-    # Populate sorted indecies list
-    for i in range(len(df)):
-        indexNums.append(i)
-    
-    # Randomize index list
-    for i in range(len(df)):
-        randRow = random.choice(indexNums)
-        # Add random index to the column
-        randIndecies.append(randRow)
-        # Remove that option from future indecies
-        indexNums.remove(randRow)
+    def randomShuffle(self):
+        df = self.copy()
+        # Make a list to hold the indecies (sorted and random)
+        indexNums = []
+        randIndecies = []
 
-    # Add the randomized list to the df as a new column
-    df['randIndex'] = pd.Series(randIndecies)
+        # Populate sorted indecies list
+        for i in range(len(df)):
+            indexNums.append(i)
 
-    # Sort the dataframe by the new index and drop that 
-    df = df.sort_values('randIndex').drop(columns='randIndex')
+        # Randomize index list
+        for i in range(len(df)):
+            randRow = random.choice(indexNums)
+            # Add random index to the column
+            randIndecies.append(randRow)
+            # Remove that option from future indecies
+            indexNums.remove(randRow)
 
-    # Reset the index column
-    df = df.reset_index().drop(columns='index')
+        # Add the randomized list to the df as a new column
+        df['randIndex'] = pd.Series(randIndecies)
 
-    # Return the updated dataframe
-    return df
+        # Sort the dataframe by the new index and drop that
+        df = df.sort_values('randIndex').drop(columns='randIndex')
+
+        # Reset the index column
+        df = df.reset_index().drop(columns='index')
+
+        # Return the updated dataframe
+        return df
